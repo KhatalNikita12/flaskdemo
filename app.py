a2bb9b32a12f2d0bb1a51ef8125e9c18190ccdea
KhatalNikita12 <Khatalnikita2003@gmail.com>
2024-10-25 00:22:06 +0530

    change in index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,21 +8,27 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
+    fname = db.Column(db.String(200), nullable=False)
+    lname = db.Column(db.String(200), nullable=False)
 
 
 with app.app_context():
     db.create_all()
 
 def __repr__(self) -> str:
-    return f"{self.sno}: {self.name}"
+    return f"{self.sno}: {self.fname} {self.lname}"
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    todo=Todo(name="Nikita")
-    db.session.add(todo)
-    db.session.commit()
-    return render_template('index.html')
+    if request.method=='POST':
+        fname=request.form['fname']
+        lname=request.form['lname']
+        todo=Todo(fname=fname,lname=lname)
+        
+        db.session.add(todo)
+        db.session.commit()
+    all_todo=Todo.query.all()
+    return render_template('index.html',all_todo=all_todo)
 
 @app.route('/show')
 def show():
